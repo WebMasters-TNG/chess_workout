51f9ac069f105d9d13c7f74017d143bf27999ed9
Andrew Smith <andrewsmith@Andrews-iMac.local>
2016-02-16 16:51:14 -0500

    Add notes for games.js and PiecesController

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -11,6 +11,7 @@ class PiecesController < ApplicationController
     @piece = Piece.find(params[:id])
     if @piece.valid_move?(piece_params, @piece)
       current_piece.update_attributes(piece_params)
+      # Send a message back to the JS after the update (after the data object is defined in the AJAX request) to confirm successful update or an error:
       respond_to do |format|
         format.js { render json: {success: true, status: :success} }
       end
